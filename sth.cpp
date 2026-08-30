68d2544f491360891081dbcbabe2cf267b2b9a95
mk_akiyama256 <ngminhkhang246@gmail.com>
2026-08-30 19:06:04 +0700

    Add files via upload

---
+++ sth.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    const string s = "!NO-K evol I";
+    for (int i = static_cast<int>(s.length()) - 1; i >= 0; i--) {
+        cout << s[i];
+    }
+    return 0;
+}
